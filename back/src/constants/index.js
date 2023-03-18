4047b95592768268b1412890526af740a348542c
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-18 11:03:47 -0700

    ✨ get all todos

--- back/src/constants/index.js
+++ back/src/constants/index.js
@@ -1,7 +1,15 @@
-const DB_URL = "mongodb://localhost:27017/todo_List";
+const DB_URL = "mongodb://localhost:27017";
+const DB_NAME = "todo_List";
 const SERVER_PORT = 3500;
+
+const COLLECTIONS = {
+  TODOS: "todos",
+  USERS: "users",
+};
 
 module.exports = {
   DB_URL,
   SERVER_PORT,
+  COLLECTIONS,
+  DB_NAME,
 };
--- back/src/controller/todos.controller.js
+++ back/src/controller/todos.controller.js
@@ -1,7 +1,34 @@
+const DB = require("../config/db");
+const { COLLECTIONS } = require("../constants");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+
 class TodosController {
-  getAll(req, res) {
-    res.write("todos");
-    res.end();
+  async getAll(req, res) {
+    try {
+      const db = await new DB().mongo();
+
+      const todos = await db.collection(COLLECTIONS.TODOS).find({}).toArray();
+
+      res.write(
+        JSON.stringify({
+          success: true,
+          statusCode: 200,
+          data: todos,
+        })
+      );
+      res.end();
+    } catch (e) {
+      console.log(e);
+      res.writeHead(StatusCodes.INTERNAL_SERVER_ERROR);
+      res.write(
+        JSON.stringify({
+          success: false,
+          statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        })
+      );
+      res.end();
+    }
   }
 
   create(req, res) {
