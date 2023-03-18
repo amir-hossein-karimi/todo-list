2723b03d9ceca9a2dda2e6b0c88b06d929b82e77
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-18 12:03:23 -0700

    ♻️  refactor codes

--- back/src/routes/auth.routes.js
+++ back/src/routes/auth.routes.js
@@ -1,9 +1,18 @@
 const { AuthController } = require("../controller/auth.controller");
 
 const authRoutes = {
-  "/register": { controller: AuthController.register, method: "post" },
-  "/login": { controller: AuthController.login, method: "post" },
-  "/refreshToken": { controller: AuthController.refreshToken, method: "post" },
+  "/register": {
+    controller: AuthController.register,
+    method: "post",
+  },
+  "/login": {
+    controller: AuthController.login,
+    method: "post",
+  },
+  "/refreshToken": {
+    controller: AuthController.refreshToken,
+    method: "post",
+  },
 };
 
 module.exports = authRoutes;
--- back/src/routes/todos.routes.js
+++ back/src/routes/todos.routes.js
@@ -4,21 +4,19 @@ const todosRoutes = {
   "/all": {
     controller: TodosController.getAll,
     method: "get",
-    preHandler: [
-      (req, res, done) => {
-        console.log("preHandler 1");
-        res.write("this is prehandler 1");
-        res.end();
-        done();
-      },
-      (req, res) => {
-        console.log("preHandler 2");
-      },
-    ],
   },
-  "/create": { controller: TodosController.create, method: "post" },
-  "/update": { controller: TodosController.update, method: "put" },
-  "/delete": { controller: TodosController.delete, method: "delete" },
+  "/create": {
+    controller: TodosController.create,
+    method: "post",
+  },
+  "/update": {
+    controller: TodosController.update,
+    method: "put",
+  },
+  "/delete": {
+    controller: TodosController.delete,
+    method: "delete",
+  },
 };
 
 module.exports = todosRoutes;
