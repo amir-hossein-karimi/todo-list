e5f380a8cd7be0afd647152caf5829a3f4660471
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-20 01:29:15 -0700

    change base route messages

--- back/src/controller/index.js
+++ back/src/controller/index.js
@@ -8,13 +8,15 @@ const baseRouteSchema = {
       console.log("this is second pre handler, its send data and stop running");
 
       // comment bottom codes to run controller
-      res.write("this is base route(from second pre handler)");
+      res.write("this is base route (from second pre handler)");
       res.end();
       done();
     },
   ],
   controller: (req, res) => {
-    console.log("this is controller, request is stoped you cant see this");
+    console.log(
+      "this is controller, request is stoped on second pre handler then you cant see this"
+    );
     res.write("this is base route (from controller)");
     res.end();
   },
