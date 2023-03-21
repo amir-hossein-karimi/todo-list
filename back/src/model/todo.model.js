38f195a3f2bbaaa18d1cc3077af2f6fba1b8522e
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-21 14:09:10 -0700

    ✨ add create todo at todo model

--- back/src/model/todo.model.js
+++ back/src/model/todo.model.js
@@ -1,5 +1,7 @@
+const { ReasonPhrases, StatusCodes } = require("http-status-codes");
 const { TYPES, TODO_STATUS, COLLECTIONS } = require("../constants");
 const { collectionInstance } = require("../utils/collectionInstance");
+const validationBySchema = require("../validators/schema.validator");
 
 const todoSchema = {
   title: { type: TYPES.STRING, required: true },
@@ -9,6 +11,7 @@ const todoSchema = {
     defaultValue: TODO_STATUS.TODO,
     acceptdValues: [...Object.values(TODO_STATUS)],
   },
+  userId: { type: TYPES.OBJECT_ID, required: true },
 };
 
 class TODO {
@@ -24,7 +27,25 @@ class TODO {
 
   getById(id) {}
 
-  create(data) {}
+  async create(data) {
+    const { error, value } = validationBySchema(data, todoSchema);
+
+    if (error) {
+      throw { message: error, statusCode: StatusCodes.BAD_REQUEST };
+    }
+
+    const todoModel = await collectionInstance(COLLECTIONS.TODOS);
+    const createRes = await todoModel.insertOne(value);
+
+    if (createRes?.acknowledged) {
+      return { message: "todo created successfully" };
+    } else {
+      throw {
+        message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+        statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+      };
+    }
+  }
 
   update(findBy, replaceData) {}
 
