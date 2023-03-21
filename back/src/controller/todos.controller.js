0629d70f7bc650223d028d9930bb233f59767e9c
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-21 14:09:25 -0700

    ✨ create todo controller

--- back/src/controller/todos.controller.js
+++ back/src/controller/todos.controller.js
@@ -1,11 +1,12 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { createError } = require("../errors");
 const TODO = require("../model/todo.model");
+const { createTodoValidatorSchema } = require("../validators/todos.validator");
 
 class TodosController {
   async getAll(req, res) {
     try {
-      const todos = await new TODO().all();
+      const todos = await new TODO().all({ userId: req.user._id });
 
       res.write(
         JSON.stringify({
@@ -24,9 +25,30 @@ class TodosController {
     }
   }
 
-  create(req, res) {
-    res.write("create");
-    res.end();
+  async create(req, res) {
+    try {
+      const dataValue = await createTodoValidatorSchema.validateAsync(req.body);
+
+      const createRes = await new TODO().create({
+        ...dataValue,
+        userId: req.user._id,
+      });
+
+      res.write(
+        JSON.stringify({
+          success: true,
+          statusCode: 201,
+          data: createRes,
+        })
+      );
+      return res.end();
+    } catch (error) {
+      return createError(
+        res,
+        error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 
   update(req, res) {
