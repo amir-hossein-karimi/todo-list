1ea3bfac997b01ede38d59c4313ec42544aea82f
amir <a.hosseinkarimi82@gmail.com>
2023-03-21 02:10:03 -0700

    ✨ add check login prehandler to todos routes

--- back/src/routes/todos.routes.js
+++ back/src/routes/todos.routes.js
@@ -1,20 +1,25 @@
 const { TodosController } = require("../controller/todos.controller");
+const checkLogin = require("../middlewares/checkLogin");
 
 const todosRoutes = {
   "/all": {
     controller: TodosController.getAll,
+    preHandler: [checkLogin],
     method: "get",
   },
   "/create": {
     controller: TodosController.create,
+    preHandler: [checkLogin],
     method: "post",
   },
   "/update": {
     controller: TodosController.update,
+    preHandler: [checkLogin],
     method: "put",
   },
   "/delete": {
     controller: TodosController.delete,
+    preHandler: [checkLogin],
     method: "delete",
   },
 };
