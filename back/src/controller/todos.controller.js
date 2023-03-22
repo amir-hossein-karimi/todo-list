bbbb6c4ca743d004144f339e847cf173654c282f
amir-hossein-karimi <a.hosseinkarimi82@gmail.com>
2023-03-22 02:58:41 -0700

    ✨ delete todo controller

--- back/src/controller/todos.controller.js
+++ back/src/controller/todos.controller.js
@@ -126,9 +126,37 @@ class TodosController {
     }
   }
 
-  delete(req, res) {
-    res.write("delete");
-    res.end();
+  async delete(req, res) {
+    try {
+      const id = req.params.get("id");
+
+      if (!id) {
+        throw {
+          message: "id is required",
+          statusCode: StatusCodes.BAD_REQUEST,
+        };
+      }
+
+      const deleteRes = await new TODO().delete({
+        _id: id,
+        userId: req.user._id,
+      });
+
+      res.write(
+        JSON.stringify({
+          success: true,
+          statusCode: 200,
+          data: deleteRes,
+        })
+      );
+      return res.end();
+    } catch (error) {
+      return createError(
+        res,
+        error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        error.message || ReasonPhrases.INTERNAL_SERVER_ERROR
+      );
+    }
   }
 }
 
