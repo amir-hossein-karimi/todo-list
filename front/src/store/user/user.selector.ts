ca255aa9434795eae4b6f0694900d10b5f4ec3c0
amir <a.hosseinkarimi82@gmail.com>
2023-06-23 03:34:10 -0700

    add user selector

--- front/src/store/user/user.selector.ts
+++ front/src/store/user/user.selector.ts
@@ -2,3 +2,5 @@ import { rootReducerInterface as stateType } from "../reducers";
 
 export const authSelector = (state: stateType) =>
   state.user.token !== undefined;
+
+  export const userSelector = (state: stateType) => state.user
