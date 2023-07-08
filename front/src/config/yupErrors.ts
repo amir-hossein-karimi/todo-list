4abfafddce63b5e7c58e55c9f97aa3862aef9928
amir <a.hosseinkarimi82@gmail.com>
2023-07-08 11:19:07 +0330

    refactor code

--- front/src/config/yupErrors.ts
+++ front/src/config/yupErrors.ts
@@ -2,16 +2,10 @@ import * as yup from "yup";
 
 yup.setLocale({
   mixed: {
-    required: (e) => {
-      return `${e.path} is required`;
-    },
+    required: (e) => `${e.path} is required`,
   },
   string: {
-    min: (e) => {
-      return `${e.path} should have a minimum length of ${e.min}`;
-    },
-    max: (e) => {
-      return `${e.path} should have a minimum length of ${e.max}`;
-    },
+    min: (e) => `${e.path} should have a minimum length of ${e.min}`,
+    max: (e) => `${e.path} should have a minimum length of ${e.max}`,
   },
 });
