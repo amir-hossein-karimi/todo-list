69095ec3fff534e777c411577333d3695e35570c
amir <a.hosseinkarimi82@gmail.com>
2023-07-09 15:22:01 +0330

    fix corse error

--- back/src/config/routes.js
+++ back/src/config/routes.js
@@ -22,7 +22,7 @@ const configRoutes = (req, res, routeObject, callCount = 0) => {
 
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Methods", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
 
   if (`/${url}` in routes) {
