bc757ed26875cfa20e0ba28d897f7a83b79dc9a3
amir <a.hosseinkarimi82@gmail.com>
2023-07-12 10:35:53 +0330

    add dialog files

--- front/src/components/Todos/AddTodo/AddTodoDialog/index.tsx
+++ front/src/components/Todos/AddTodo/AddTodoDialog/index.tsx
@@ -1,7 +1,20 @@
-import { Dialog } from "@mui/material";
+import { Box, Dialog } from "@mui/material";
+import { FC } from "react";
+import useStyles from "./useStyles";
 
-const AddTodoDialog = () => {
-  return <Dialog open>test</Dialog>;
+interface addTodoDialogProps {
+  open: boolean;
+  toggleDialog: () => void;
+}
+
+const AddTodoDialog: FC<addTodoDialogProps> = ({ open, toggleDialog }) => {
+  const classes = useStyles();
+
+  return (
+    <Dialog open={open} onClose={toggleDialog}>
+      <Box className={classes.container}>test</Box>
+    </Dialog>
+  );
 };
 
 export default AddTodoDialog;
