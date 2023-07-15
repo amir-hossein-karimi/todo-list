0593d501221ae3ce7723f56c34f9914cafc0fd94
amir <a.hosseinkarimi82@gmail.com>
2023-07-15 12:17:15 +0330

    delete and edit icons add

--- front/src/components/Todos/TodoItem/index.tsx
+++ front/src/components/Todos/TodoItem/index.tsx
@@ -5,21 +5,41 @@ import {
   AccordionDetails,
   AccordionProps,
   AccordionSummary,
+  Box,
+  Button,
   Typography,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 
 import { todoType } from "../../../types";
+import useStyles from "./useStyles";
 
 interface TodoItemProps extends Omit<AccordionProps, "children"> {
   todo: todoType;
 }
 
 const TodoItem: FC<TodoItemProps> = ({ todo, ...props }) => {
+  const classes = useStyles();
+
   return (
     <Accordion {...props}>
       <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-        <Typography>{todo.title}</Typography>
+        <Box className={classes.summary}>
+          <Typography>{todo.title}</Typography>
+
+          <Box className={classes.actionButtons}>
+            <Button variant="text">
+              <DeleteIcon color={"error"} />
+            </Button>
+
+            <Button variant="text">
+              <EditIcon color={"secondary"} />
+            </Button>
+          </Box>
+        </Box>
       </AccordionSummary>
+
       <AccordionDetails>
         <Typography>{todo.description}</Typography>
       </AccordionDetails>
--- front/src/components/Todos/TodoItem/useStyles.ts
+++ front/src/components/Todos/TodoItem/useStyles.ts
@@ -1,7 +1,20 @@
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles(() => ({
-  container: {},
+  summary: {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+
+  actionButtons: {
+    display: "flex",
+
+    "& > button": {
+      padding: "0",
+      minWidth: "2.5rem",
+    },
+  },
 }));
 
 export default useStyles;
